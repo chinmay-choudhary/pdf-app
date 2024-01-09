1f1b4cafef48d308c68826a49ed0b1ef14e0e049
chinmay-choudhary <chinmay.ch33@gmail.com>
2024-01-09 23:17:20 +0530

    pylint changes

--- app.py
+++ app.py
@@ -10,13 +10,10 @@ def merge():
     zipFile = request.files.get('zip')
     if zipFile:
         mergedPdfPath = unzipAndMerge(zipFile)
-
         if mergedPdfPath:
             response = send_file(mergedPdfPath, as_attachment=True)
             os.remove(mergedPdfPath)
             return response
-    else:
-        return {"STATUS": 400, "message": "No zip file provided"}
 
 if __name__ == '__main__':
     app.run(port=8000, host="0.0.0.0", debug=True)
--- test.py
+++ test.py
@@ -1,15 +1,15 @@
 import requests
 
-# apiUrl of your merge API
-apiUrl = 'http://localhost:8000/merge'
+# API_URL of your merge API
+API_URL = 'http://localhost:8000/merge'
 
 # Path to the zip file you want to send
-filePath = 'Archive.zip'
+FILE_PATH = 'Archive.zip'
 
 # Open the file in binary mode and send it as part of a multipart-form data
-with open(filePath, 'rb') as f:
-    files = {'zip': (filePath, f, 'application/zip')}
-    response = requests.post(apiUrl, files=files,timeout=25)
+with open(FILE_PATH, 'rb') as f:
+    files = {'zip': (FILE_PATH, f, 'application/zip')}
+    response = requests.post(API_URL, files=files,timeout=25)
 
 # Check if the request was successful
 if response.status_code == 200:
